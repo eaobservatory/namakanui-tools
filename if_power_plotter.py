85dc3bdd3d5ea55e6fe37f4f593a79c3001e2912
Akil Marshall <a.marshall@eaobservatory.org>
2023-02-10 12:05:46 -1000

    properly implemented the difference operations (plotting and data file output) for attenuation tables

--- if_power_plotter.py
+++ if_power_plotter.py
@@ -3,7 +3,7 @@
 IF Power Plotter
 """
 from lib.parser import if_power
-from lib.util import fancy_title
+from lib.util import fancy_title, ASCII_NAMES
 import pandas as pd
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
@@ -16,22 +16,62 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     path = args.data_file
-    # title = args.title
+    diff = args.diff
     csv = args.csv
 
-    rx = path.parent.parent.stem
 
-    df = pd.read_csv(path, sep=',', engine='python')
-    if csv:
-        csv_name = f'if_power_{rx}_{path.stem}{path.suffix}.csv'
-        df.to_csv(csv_name, index=False)
-        print(f'produced: {csv_name}')
+    if diff:
+        # do the difference
+        data_set_a = '.'.join(path.stem.split('.')[1:])
+        data_set_b = '.'.join(diff.stem.split('.')[1:])
+        data_name = f'{data_set_b}->{data_set_a}'
+        rx = path.stem.split('_')[0]
+        fname = f'att_change_{ASCII_NAMES[rx]}_{data_name}'
+        df = pd.read_csv(path, sep=' ', skipfooter=1, engine='python')
+        df_diff = pd.read_csv(diff, sep=' ', skipfooter=1, engine='python')
+        df_out = pd.DataFrame()
+        df_out['lo_ghz'] = df['#lo_ghz']
+        df_out['change'] = df.att - df_diff.att
+        if csv:
+            # difference CSV
+            fname += '.csv'
+            df_out.rename(inplace=True, columns={'lo_ghz': 'LO (GHz)', 'change': 'Attenuation Change (8bit counts)'})
+            df_out.to_csv(fname, index=False)
+            print(fname)
+            exit()
 
-    plt.scatter(df.lo_ghz, df.if_power)
-    plt.xlabel('LO (GHz)')
-    plt.ylabel('IF Power (V)')
-    name = f'if_power_{rx}_{path.stem}{path.suffix}.png'
-    title = f'IF Power {fancy_title(rx)}'
-    plt.title(title)
-    plt.savefig(name)
-    print(f'produced: {name}')
+        positive = df_out  # attenuation increased
+        negative = df_out[df_out.change < 0]  # attenuation decreased
+        plt.scatter(positive.lo_ghz, positive.change, s=2, color='g')
+        plt.scatter(negative.lo_ghz, negative.change, s=2, color='r')
+        plt.axhline(color='black')
+        plt.title(f'{fancy_title(rx)} New_Att - Old_Att')
+        plt.ylabel('att change')
+        plt.xlabel('LO (GHz)')
+        fname += '.png'
+        plt.savefig(fname)
+        print(fname)
+        exit()
+
+    else:
+        # normal plotting
+        good_range = (-2.5, -0.5)
+        df = pd.read_csv(path, sep=',', engine='python')
+        data_set = f'{path.stem}{path.suffix}'
+        rx = path.parent.parent.stem
+        title = f'IF Power {fancy_title(rx)}'
+        fname = f'if_power_{rx}_{data_set}' 
+
+        if csv:
+            fname += '.csv'
+            df.to_csv(fname, index=False)
+            print(fname)
+            exit()
+        plt.scatter(df.lo_ghz, df.if_power)
+        plt.xlabel('LO (GHz)')
+        plt.ylabel('IF Power (V)')
+        plt.title(title)
+        fname += '.png'
+        plt.savefig(fname)
+        print(fname)
+        exit()
