b34e4e9f5eabbc744bea0f87b69f1b2c0869a796
Akil Marshall <a.marshall@eaobservatory.org>
2023-02-22 11:46:34 -1000

    changed file name

---
+++ lib/extras.py
@@ -0,0 +1,32 @@
+__all__ = ['fancy_title', 'HAWAIIAN_NAMES', 'ALMA_BAND', 'ASCII_NAMES']
+
+ASCII = ['alaihi', 'uu', 'aweoweo'] 
+BANDS = ['3', '6', '7']
+ABBR_BAND_NAMES = ['b3', 'b6', 'b7']
+HAWAIIAN = ['‘Ala‘ihi', '‘Ū‘ū', '‘Āweoweo']
+
+'''
+b3     -> Ala'ihi
+alaihi -> Ala'ihi
+       ...
+b7     -> U'u
+uu     -> U'u
+'''
+HAWAIIAN_NAMES = {**dict(zip(ABBR_BAND_NAMES, HAWAIIAN)), **dict(zip(ASCII, HAWAIIAN))}
+'''
+3 -> Ala'ihi
+  ...
+7 -> U'u
+'''
+ALMA_BAND = {**dict(zip(ABBR_BAND_NAMES, BANDS)), **dict(zip(ASCII, BANDS))}
+'''
+b3     -> alaihi
+       ...
+b7     -> uu
+'''
+ASCII_NAMES = {**dict(zip(ABBR_BAND_NAMES, ASCII)), **dict(zip(BANDS, ASCII))}
+
+
+def fancy_title(rx: str):
+    if rx in ASCII or rx in ABBR_BAND_NAMES or rx in BANDS:
+        return f'{HAWAIIAN_NAMES[rx]} (band {ALMA_BAND[rx]})'
